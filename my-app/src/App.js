2230554cbcf49be47b0ce645350ee22eab1e3f5e
Vedika Chowdhary <vedikachowdhary2003@gmail.com>
2024-03-18 12:42:28 +0530

    included app and index files along with images

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import HomePage from './Components/HomePage';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
