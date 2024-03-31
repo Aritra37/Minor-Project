5b617d4389a0f64a8514197fa599cb9d7a043d55
Vedika Chowdhary <vedikachowdhary2003@gmail.com>
2024-03-31 19:41:10 +0530

    created frame alert group and app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
 import HomePage from './Components/HomePage';
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <HomePage />
+      <Footer />
     </div>
   );
 }
