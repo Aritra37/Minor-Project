372ea4a3a31ddda60459e7884bc1dcf237b9f534
Vedika Chowdhary <vedikachowdhary2003@gmail.com>
2024-03-31 19:49:46 +0530

    added success files

---
+++ my-app/src/Components/Success.js
@@ -0,0 +1,42 @@
+import "./Success.css";
+
+
+const Success = ({
+    id,
+    heading,
+    text,
+    image,
+  }) => {
+  return (
+    <div className="successtextFrame">
+
+      <div className="helpingHandFrame">
+        {id%2===0? <img
+            className="helpingHandFrameChild evenImage"
+            loading="lazy"
+            alt=""
+            src={image}
+          />:<></>}
+        <div className="successtextFrame1">
+          <div className="groupFrame">
+            <b className={id%2===0? `loremForestfireEven`:`loremForestfire`}>{heading}</b>
+            <div className="currentDisaster">
+              <div className= {id%2===0? `loremIpsumDolorEven`:`loremIpsumDolor`}>
+                {text}
+              </div>
+            </div>
+          </div>
+        </div>
+        {id%2!=0? <img
+            className="helpingHandFrameChild"
+            loading="lazy"
+            alt=""
+            src={image}
+          />:<></>}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Success;
