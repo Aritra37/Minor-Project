048ae542d9cbec5461dbd3481c493ff68888800e
Vedika Chowdhary <vedikachowdhary2003@gmail.com>
2024-04-06 21:07:54 +0530

    updated and created files

---
+++ my-app/src/Components/HomeScreen.js
@@ -0,0 +1,13 @@
+import HomePage from "./HomePage";
+import Header from "./Header";
+import Footer from "./Footer";
+const HomeScreen = () => {
+    return (
+    <>
+    <Header/>
+    <HomePage/>
+    <Footer />
+    </>
+    );
+}
+export default HomeScreen;
